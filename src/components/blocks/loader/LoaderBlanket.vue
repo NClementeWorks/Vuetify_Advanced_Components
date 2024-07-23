dae31f9fe00009ed77302b9a9c2231c236a340c4
Noliani Clemente Hernandez <44977963+nclemente@users.noreply.github.com>
2024-07-23 17:05:22 -0400

    Add LoaderBlanket component

---
+++ src/components/blocks/loader/LoaderBlanket.vue
@@ -0,0 +1,32 @@
+<!-- LoaderBlanket -->
+
+<script setup>
+  const props = defineProps({
+    status: {
+      type: Number,
+      default: 0,
+      required: false,
+    },
+    message: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  })
+</script>
+
+<template>
+  
+  <div
+    v-if="status > 0"
+    class="loader_blanket"
+    >
+    <VProgressCircular
+      :size="50"
+      />
+    <span class="text-center font-weight-bold mt-4"
+      v-html="message"
+      ></span>
+  </div>
+
+</template>
